cf1e453c2e0afbfc2aa7b5bd1bd5dcfe1f9929a8
Viral Vaghela <viralvaghela8577@gmail.com>
2024-10-11 09:43:18 +0530

    Added lightweight-charts package, implemented candlestick chart functionality, and other enhancements

--- src/components/stocks/StockChart.js
+++ src/components/stocks/StockChart.js
@@ -5,63 +5,38 @@ const StockChart = ({ candles }) => {
   const chartContainer = useRef(null);
 
   useEffect(() => {
-    if (chartContainer.current) {
-      const chartOptions = {
-        layout: {
-          textColor: 'white',
-          background: {
-            type: 'solid',
-            color: 'white',
-          },
-        },
-        width: chartContainer.current.clientWidth,
-        height: chartContainer.current.clientHeight,
-      };
+    if (!chartContainer.current) return;
 
-      const chartRef = chartContainer.current;
-      const chart = createChart(chartRef, chartOptions);
+    const chart = createChart(chartContainer.current, {
+      layout: { textColor: 'white', background: { color: 'white' } },
+      width: chartContainer.current.clientWidth,
+      height: chartContainer.current.clientHeight,
+    });
 
-      const candlestickSeries = chart.addCandlestickSeries({
-        upColor: '#0A9981',
-        downColor: '#F23545',
-        borderVisible: false,
-        wickUpColor: '#0A9981',
-        wickDownColor: '#F23545',
-      });
+    const candlestickSeries = chart.addCandlestickSeries({
+      upColor: '#0A9981', downColor: '#F23545',
+      wickUpColor: '#0A9981', wickDownColor: '#F23545',
+    });
 
-      // Check if candles is not undefined or null
-      if (candles) {
-        // Map the candles data to the format required by lightweight-charts
-        const formattedCandles = Object.values(candles).map(candle => ({
-          time: candle.startAt.seconds, // Assuming startAt is a Firestore Timestamp
-          open: candle.open,
-          high: candle.high,
-          low: candle.low,
-          close: candle.close,
-        }));
+    if (candles) {
 
-        // Sort the data by time in ascending order
-        formattedCandles.sort((a, b) => a.time - b.time);
+      // Convert candles object into array of values..
+      const formattedCandles = Object.values(candles)
+        .map(({ startAt, open, high, low, close }) => ({
+          time: startAt.seconds, open, high, low, close
+        }))
+        .sort((a, b) => a.time - b.time);
 
-        // Set the data for the candlestick series
-        candlestickSeries.setData(formattedCandles);
-
-        chart.timeScale().fitContent();
-      }
-
-      return () => chart.remove();
+      candlestickSeries.setData(formattedCandles);
+      chart.timeScale().fitContent();
     }
+
+    return () => chart.remove();
   }, [candles]);
 
   return (
-    <div
-      ref={chartContainer}
-      className='custom-cursor'
-      id='stock-chart'
-      style={{ width: '100%', height: '400px' }}
-    >
-    </div>
+    <div className="custom-cursor" ref={chartContainer} style={{ width: '100%', height: '400px' }} />
   );
-}
+};
 
 export default StockChart;
